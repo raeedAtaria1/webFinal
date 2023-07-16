22f05426ae020c5597eddd4ce5b902c9e70cac6b
raeed ataria <127938200+raeedAtaria1@users.noreply.github.com>
2023-07-16 16:27:36 +0300

    Add files via upload

---
+++ frontEnd/jsFiles/sigh_up.js
@@ -0,0 +1,45 @@
+  document.addEventListener('DOMContentLoaded', () => {
+    const workerCheckbox = document.getElementById('workerCheckbox');
+    const adminEmailField = document.getElementById('adminEmailField');
+
+    workerCheckbox.addEventListener('change', () => {
+      if (workerCheckbox.checked) {
+        adminEmailField.style.display = 'block'; // Show the admin email field
+        document.getElementById('InputAdminMail').setAttribute('required', ''); // Make it mandatory
+      } else {
+        adminEmailField.style.display = 'none'; // Hide the admin email field
+        document.getElementById('InputAdminMail').removeAttribute('required'); // Make it optional
+      }
+    });
+  });
+
+const form = document.getElementById('signupForm');
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const fullName = form.elements.fullName.value;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+    const adminEmail = form.elements.InputAdminMail.value || 'noAdmin';
+    const role="user";
+
+    try {
+    const response = await fetch('/signup', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ fullName, email, password,role,adminEmail})
+    });
+    if (response.ok) {
+        localStorage.setItem('email', email);
+        window.location.href = '/userHomePage.html';
+    } else {
+        const errorMessage = await response.text();
+        alert(errorMessage);
+    }
+    } catch (error) {
+    console.error(error);
+    console.error("Something went wrong");
+    }
+});
